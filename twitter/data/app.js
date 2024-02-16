8685c83019e72ed1b8cd407d0881a433c7e58586
Taichiro Kosugi <kosugi@darakeru.com>
2024-02-16 23:29:57 +0900

    [add]Add Twitter Log

---
+++ twitter/data/app.js
@@ -0,0 +1,495 @@
+window.YTD.app.part0 = [
+  {
+    "app" : {
+      "appId" : "1016318735",
+      "appNames" : [
+        "アイドルマスター シンデレラガールズ スターライトステージ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1058959277",
+      "appNames" : [
+        "Uber Eats: Food Delivery",
+        "Uber Eats: Delivery de comida",
+        "Uber Eats: Comida a domicilio",
+        "Uber Eats: Yemek Teslimatı",
+        "Uber Eats: matleverans",
+        "Uber Eats : Livraison de repas",
+        "Uber Eats: ruokatoimitukset",
+        "Uber Eats 优食：餐点派送",
+        "Uber Eats: Få maten levert",
+        "Uber Eats:Penghantaran Makanan",
+        "Uber Eats: Essenslieferung",
+        "Uber Eats - Comida a Domicilio",
+        "Uber Eats - 우버 이츠",
+        "Uber Eats maaltijdbezorging",
+        "Uber Eats（ウーバーイーツ) 出前/フードデリバリー",
+        "Uber Eats: การจัดส่งอาหาร",
+        "Uber Eats: Ứng dụng đặt món ăn",
+        "Uber Eats: Consegna di cibo",
+        "Uber Eats: dostawa jedzenia",
+        "Uber Eats: Madlevering",
+        "Uber Eats: доставка еды",
+        "Uber Eats: Pengantaran Makanan"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1074866833",
+      "appNames" : [
+        "ABEMA(アベマ) 新しい未来のテレビ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1195834442",
+      "appNames" : [
+        "バンドリ！ ガールズバンドパーティ！"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1325457827",
+      "appNames" : [
+        "ウマ娘 プリティーダービー"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1351283922",
+      "appNames" : [
+        "ヤンジャン！",
+        "ヤンジャン！人気コミックが読める漫画アプリ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1351704611",
+      "appNames" : [
+        "漫画 ebookjapan　電子書籍コミック・まんが本棚"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1377018522",
+      "appNames" : [
+        "ラブライブ！スクールアイドルフェスティバルALL STARS"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1469186379",
+      "appNames" : [
+        "雀魂 -じゃんたま-"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1480809171",
+      "appNames" : [
+        "Mildom(ミルダム) ゲーム実況・ライブ配信アプリ",
+        "Mildom(ミルダム)　ゲーム実況動画・ライブ配信アプリ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "1490381755",
+      "appNames" : [
+        "D4DJ Groovy Mix(グルミク)"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "288091002",
+      "appNames" : [
+        "GYAO! / ギャオ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "324684580",
+      "appNames" : [
+        "Spotify: Hudba a podcasty",
+        "Spotify – muzică și podcasturi",
+        "Spotify - Music and Podcasts",
+        "Spotify Music",
+        "Spotify: Objevuj novou hudbu",
+        "Spotify: Música em streaming",
+        "Spotify - Música y podcasts",
+        "Spotify: музика та подкасти",
+        "Spotify: Müzik dinle ve indir",
+        "Spotify – Musik och podcasts",
+        "Spotify: موسيقى وبودكاست تحبها",
+        "Spotify : Musique et podcasts",
+        "Spotify – musiikki + podcastit",
+        "Spotify - 音樂和 Podcast",
+        "Spotify – Musikk og podkaster",
+        "Spotify - Muzik dan Podcast",
+        "Spotify: Musik und Podcasts",
+        "Spotify: escucha música y más",
+        "Spotify (스포티파이)",
+        "Spotify - Muziek en podcasts",
+        "Spotify: 音楽とポッドキャスト",
+        "Spotify - เพลย์ลิสต์เพลงโปรด",
+        "Spotify - Phát nhạc playlist",
+        "Spotify: Fedezz fel új zenéket",
+        "Spotify: musica e podcast",
+        "Spotify: odkryj nową muzykę",
+        "Spotify – glazba i podcasti",
+        "Spotify - Μουσική και podcast",
+        "Spotify: New music & podcasts",
+        "Spotify: musik og podcasts",
+        "Spotify: музыка и подкасты",
+        "Spotify: mainkan musik baru"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "333903271",
+      "appNames" : [
+        "Twitter",
+        "Twitter -트위터",
+        "Twitter ツイッター",
+        "Twitter - ทวิตเตอร์",
+        "Twitter - твиттер"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "354976659",
+      "appNames" : [
+        "はてなブックマーク",
+        "Hatena Bookmark"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "370515585",
+      "appNames" : [
+        "radiko"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "405489166",
+      "appNames" : [
+        "出前館-フードデリバリー"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "419267350",
+      "appNames" : [
+        "楽天市場"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "460177396",
+      "appNames" : [
+        "Twitch",
+        "Twitch: Live Game Streaming",
+        "Twitch: Live-Stream & Chat",
+        "Twitch: 동영상 스트리밍 라이브 시청은 트위치에서",
+        "Twitch: fortnite, pubg e CSGO"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "479516143",
+      "appNames" : [
+        "Minecraft"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "626776655",
+      "appNames" : [
+        "ラブライブ！スクールアイドルフェスティバル（スクフェス）"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "747779910",
+      "appNames" : [
+        "ミクチャ - ライブ配信 & 動画アプリ",
+        "ミクチャ - ライブ配信&動画アプリ",
+        "MIXCHANNEL 믹스채널 - Live 방송"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "875750302",
+      "appNames" : [
+        "少年ジャンプ＋ 人気漫画が読める雑誌アプリ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "912776162",
+      "appNames" : [
+        "OPENREC.tv",
+        "OPENREC.tv (オープンレック)"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "998542154",
+      "appNames" : [
+        "マガポケ - 人気マンガが毎日楽しめるコミックアプリ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "com.adobe.reader",
+      "appNames" : [
+        "Adobe Acrobat Reader: Edit PDF",
+        "Adobe Acrobat Reader pro PDF",
+        "Adobe Acrobat Reader: Ler PDF",
+        "Adobe Acrobat Reader ile PDF",
+        "Adobe Acrobat Reader för PDF",
+        "Adobe Acrobat Reader pour PDF",
+        "Adobe Acrobat Reader PDF:lle",
+        "Adobe Acrobat Reader：编辑 PDF",
+        "Adobe Acrobat Reader for PDF",
+        "Adobe Acrobat Reader für PDF",
+        "Adobe Acrobat Reader para PDF",
+        "Adobe Acrobat Reader PDF 뷰어",
+        "Adobe Acrobat Reader: PDF書類の管理",
+        "Adobe Acrobat Reader voor PDF",
+        "Adobe Acrobat Reader per PDF",
+        "Adobe Acrobat Reader do PDF-ów",
+        "Adobe Acrobat Reader для PDF",
+        "Adobe Acrobat Reader: Baca PDF"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "com.amazon.clouddrive.photos",
+      "appNames" : [
+        "Amazon Photos",
+        "Prime Photos from Amazon"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "com.hatena.android.bookmark",
+      "appNames" : [
+        "はてなブックマーク - 情報発見アプリ",
+        "Hatena Bookmark: Save, share",
+        "はてなブックマーク - 無料情報発見アプリの決定版"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "com.spotify.music",
+      "appNames" : [
+        "Spotify: Hudba a podcasty",
+        "Spotify: muzică",
+        "Spotify: Music and Podcasts",
+        "Spotify: Tónlist og hlaðvörp",
+        "Spotify: இசை & பாட்காஸ்ட்கள்",
+        "Spotify: Muziki na Podikasti",
+        "Spotify: Umculo Nama-podcast",
+        "Spotify Music",
+        "Spotify",
+        "Spotify – hudba a podcasty",
+        "Spotify: Descubra mais músicas",
+        "Spotify: muusika ja netisaated",
+        "Spotify: Free Music Streaming",
+        "Spotify: музика та подкасти",
+        "«اسپاتیفای: موسیقی و پادکست»",
+        "Spotify: Müzik ve Podcast'ler",
+        "Spotify: Musiek, podsendings",
+        "Spotify – Musik och podcasts",
+        "Spotify: الموسيقى والبودكاست",
+        "Spotify: Listen to music",
+        "Spotify​​: musique et podcasts",
+        "Spotify: musiikki ja podcastit",
+        "Spotify：音乐和播客",
+        "Spotify - Muzik dan Podcast",
+        "Spotify – Musikk og podkaster",
+        "Spotify: Musik und Podcasts",
+        "Spotify: música y pódcasts",
+        "Spotify: Musiqi & Podkastlar",
+        "Spotify: muzika + tinklalaidės",
+        "Spotify: Музика и подкасти",
+        "Spotify (스포티파이)",
+        "Spotify: 音楽やポッドキャストのコレクションを聴く。",
+        "Spotify - Muziek en podcasts",
+        "Spotify: เพลงและพอดแคสต์",
+        "Spotify — Zene és podcastok",
+        "Spotify- Âm nhạc số 1 thế giới",
+        "Spotify: glasba in podkasti",
+        "Spotify: musica e podcast",
+        "Spotify — muzyka i podcasty",
+        "Spotify - Μουσική και podcast",
+        "Spotify: glazba i podcasti",
+        "Spotify: म्यूज़िक और पॉडकास्ट",
+        "Spotify: muzika i podkasti",
+        "Spotify: mūzika, raidieraksti",
+        "Spotify – musik og podcasts",
+        "Spotify: सङ्गीत र पोडकास्ट",
+        "Spotify: संगीत आणि पॉडकास्‍ट",
+        "Spotify:സംഗീതം-പോഡ്‌കാസ്റ്റ്",
+        "Spotify: музыка и подкасты",
+        "Spotify: Musik dan Podcast",
+        "Spotify ಮ್ಯೂಸಿಕ್"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "com.twitter.android",
+      "appNames" : [
+        "Twitter",
+        "Twitter (טוויטר)",
+        "Twitter - التويتر",
+        "트위터",
+        "Twitter - ทวิตเตอร์",
+        "Твиттер"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "com.ubercab.eats",
+      "appNames" : [
+        "Uber Eats: Donáška jedla",
+        "Uber Eats: Livrări de mâncare",
+        "Uber Eats: Food Delivery",
+        "Uber Eats: Utoaji wa chakula",
+        "Uber Eats: משלוחי אוכל מקומיים",
+        "Uber Eats: Rozvoz jídla",
+        "Uber Eats: Delivery de Comida",
+        "Uber Eats: Toidu kohaletoomine",
+        "Uber Eats: доставка їжі додому",
+        "Uber Eats: Yemek & Market",
+        "Uber Eats: საკვების მიწოდება",
+        "Uber Eats: Food delivery",
+        "Uber Eats: خدمة توصيل الطعام",
+        "Uber Eats៖ ការចែកចាយអាហារ",
+        "Uber Eats: Livraison de repas",
+        "Uber Eats: Tilaa ruokaa",
+        "Uber Eats 优食：本地餐点派送",
+        "Uber Eats Penghantaran makanan",
+        "Uber Eats: Lokal matlevering",
+        "Uber Eats: Essen bestellen",
+        "Uber Eats: Comida a domicilio",
+        "Uber Eats: Qida çatdırılması",
+        "Uber Eats: Maisto pristatymas",
+        "Uber Eats - စားစရာပို့ပေးမှု",
+        "Uber Eats: доставка на храна",
+        "Uber Eats - 우버 이츠",
+        "Uber Eats: フードデリバリー 出前",
+        "Uber Eats: Eten bestellen",
+        "Uber Eats: สั่งอาหาร & ของกิน",
+        "Uber Eats: Étel szállítás",
+        "Uber Eats Ship đồ ăn rất nhanh",
+        "Uber Eats: Dostava hrane",
+        "Uber Eats: Consegna di cibo",
+        "Uber Eats: dostawa jedzenia",
+        "Uber Eats: स्थानीय भोजन वितरण",
+        "Uber Eats: pārtikas piegāde",
+        "Uber Eats: lokal madlevering",
+        "Uber Eats: доставка еды",
+        "Uber Eats: Pengantaran makanan"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.co.mcdonalds.android",
+      "appNames" : [
+        "McDonald's Japan",
+        "マクドナルド"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.co.promise.spap.android",
+      "appNames" : [
+        "プロミスのアプリローン。カードレスで即日お借入＆ご返済。"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.co.rakuten.android",
+      "appNames" : [
+        "楽天市場 ショッピングアプリ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.co.sej.app",
+      "appNames" : [
+        "セブン-イレブンアプリ"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.gocro.smartnews.android",
+      "appNames" : [
+        "SmartNews: Local Breaking News",
+        "スマートニュース　最新ニュースや天気・天気予報、クーポンも"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.ne.paypay.android.app",
+      "appNames" : [
+        "PayPay-ペイペイ(キャッシュレスでスマートにお支払い)"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "jp.radiko.Player",
+      "appNames" : [
+        "radiko"
+      ]
+    }
+  },
+  {
+    "app" : {
+      "appId" : "tv.abema",
+      "appNames" : [
+        "ABEMA（アベマ）新しい未来のテレビ"
+      ]
+    }
+  }
+]
